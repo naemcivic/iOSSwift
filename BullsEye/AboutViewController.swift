87f82d33f28758f492e819f8c85f82fab3b8798c
naemcivic <naemcivic@gmail.com>
2016-10-25 14:40:18 -0400

    complete functionality of the game

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Philip Chan on 2016-10-25.
+//  Copyright © 2016 Philip Chan. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,49 +11,89 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewRound()
         updateLabels()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func showAlert() {
-        let message = "The value of the slider is \(currentValue)"
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
         
-        let alert = UIAlertController(title: "Hello, World",
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK",
+                                   style: .default, handler: { action in
+                                                                self.startNewRound()
+                                                                self.updateLabels()
+                                                            })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        startNewRound()
-        updateLabels()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
     }
     
+    @IBAction func restart() {
+        startNewGame()
+        updateLabels()
+    }
+    
     func startNewRound() {
+        round += 1
         targetValue = 1 + Int(arc4random_uniform(100))
-        currentValue = 25
+        currentValue = 50
         slider.value = Float(currentValue)
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        
+        startNewRound()
     }
 }
-
